c0d73d5e9fddc55467db379dab175d9d66275f9e
ville-solja <ville.solja@gmail.com>
2026-03-12 13:49:04 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/ville-solja,
+      GitHub: "https://github.com/ville-solja",
     },
   }),
 }
